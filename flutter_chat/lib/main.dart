db362108372a4d2e8b4e911371d2fff1ea94704a
Hemanth Raj <raj84325@gmail.com>
2018-02-01 19:37:44 +0530

    Image upload optimizations

--- flutter_chat/lib/main.dart
+++ flutter_chat/lib/main.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:math';
-import 'package:flutter/material.dart';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image/image.dart' as I;
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 final googleSignIn = new GoogleSignIn();
 final analytics = new FirebaseAnalytics();
@@ -22,8 +25,9 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Chat',
       theme: new ThemeData(
-        primarySwatch: Colors.red,
-      ),
+          primarySwatch: Colors.red,
+          textTheme:
+              new TextTheme(subhead: new TextStyle(color: Colors.white))),
       home: new ChatScreen(title: 'Flutter Chat'),
     );
   }
@@ -39,34 +43,32 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   final reference = FirebaseDatabase.instance.reference().child('messages');
 
   final TextEditingController _controller = new TextEditingController();
 
   bool _isComposing = false;
 
-  _ChatScreenState(){
+  _ChatScreenState() {
     _ensureLoggedIn();
   }
 
   Future<Null> _ensureLoggedIn() async {
     GoogleSignInAccount user = googleSignIn.currentUser;
-    if (user == null)
-      user = await googleSignIn.signInSilently();
+    if (user == null) user = await googleSignIn.signInSilently();
     if (user == null) {
       await googleSignIn.signIn();
       analytics.logLogin();
     }
-    if (await auth.currentUser() == null) {                          
-      GoogleSignInAuthentication credentials =                       
-      await googleSignIn.currentUser.authentication;                 
-      await auth.signInWithGoogle(                                   
-        idToken: credentials.idToken,                                
+    if (await auth.currentUser() == null) {
+      GoogleSignInAuthentication credentials =
+          await googleSignIn.currentUser.authentication;
+      await auth.signInWithGoogle(
+        idToken: credentials.idToken,
         accessToken: credentials.accessToken,
-      );                                                             
+      );
     }
-    setState((){});
+    setState(() {});
   }
 
   Future<Null> _handleSubmit(String text) async {
@@ -75,14 +77,14 @@ class _ChatScreenState extends State<ChatScreen> {
       _isComposing = false;
     });
     await _ensureLoggedIn();
-    _sendMessage(text:text);
+    _sendMessage(text: text);
   }
 
-  void _sendMessage({ String text , String imageUrl}) {
-    reference.push().set({                                         
+  void _sendMessage({String text, String imageUrl}) {
+    reference.push().set({
       'text': text,
       'imageUrl': imageUrl,
-      'senderName': googleSignIn.currentUser.displayName,          
+      'senderName': googleSignIn.currentUser.displayName,
       'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
       'email': googleSignIn.currentUser.email
     });
@@ -94,34 +96,45 @@ class _ChatScreenState extends State<ChatScreen> {
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: new Row(
         children: <Widget>[
-          new Container(                                           
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),     
-            child: new IconButton(                                 
-                icon: new Icon(Icons.photo_camera),                
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+                icon: new Icon(Icons.photo_camera),
                 onPressed: () async {
                   await _ensureLoggedIn();
                   File imageFile = await ImagePicker.pickImage();
-                  StorageReference ref =                                             
-                  FirebaseStorage.instance.ref().child("image_"+new DateTime.now().toIso8601String()+".jpg");
-                  StorageUploadTask uploadTask = ref.put(imageFile);                 
+                  await new Future.delayed(new Duration(milliseconds: 500));
+                  I.Image _img = I.decodeImage(imageFile.readAsBytesSync());
+                  if (_img.width > 720) {
+                    _img = I.copyResize(_img, 720);
+                  }
+                  String dir = (await getTemporaryDirectory()).path;
+                  String timeStamp = new DateTime.now().toIso8601String();
+                  File newImage = new File(dir + timeStamp + ".jpg");
+                  newImage.writeAsBytesSync(I.encodeJpg(_img));
+                  StorageReference ref = FirebaseStorage.instance
+                      .ref()
+                      .child("image_" + timeStamp + ".jpg");
+                  StorageUploadTask uploadTask = ref.put(newImage);
                   Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                  newImage.deleteSync();
                   _sendMessage(imageUrl: downloadUrl.toString());
-                }
-            ),                                                     
+                }),
           ),
           new Flexible(
               child: new TextField(
-                controller: _controller,
-                onChanged: (String text) => setState((){
+            controller: _controller,
+            onChanged: (String text) => setState(() {
                   _isComposing = text.length > 0;
                 }),
-                onSubmitted: _handleSubmit,
-                decoration:
+            onSubmitted: _handleSubmit,
+            decoration:
                 new InputDecoration.collapsed(hintText: "Start typing..."),
-              )),
+          )),
           new IconButton(
             icon: new Icon(Icons.send),
-            onPressed: _isComposing?() => _handleSubmit(_controller.text):null,
+            onPressed:
+                _isComposing ? () => _handleSubmit(_controller.text) : null,
             color: Theme.of(context).accentColor,
           )
         ],
@@ -140,18 +153,18 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Flexible(
-                child: new FirebaseAnimatedList(                            
-                  query: reference,                                       
-                  sort: (a, b) => b.key.compareTo(a.key),                 
-                  padding: new EdgeInsets.all(8.0),                       
-                  reverse: true,                                          
-                  itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation) {
-                    return googleSignIn.currentUser==null?new Container():new Message(
-                        snapshot: snapshot,                                 
-                        animation: animation                                
-                    );                                                    
-                  },                                                      
-                )),
+                child: new FirebaseAnimatedList(
+              query: reference,
+              sort: (a, b) => b.key.compareTo(a.key),
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder:
+                  (_, DataSnapshot snapshot, Animation<double> animation) {
+                return googleSignIn.currentUser == null
+                    ? new Container()
+                    : new Message(snapshot: snapshot, animation: animation);
+              },
+            )),
             const Divider(
               height: 1.0,
             ),
@@ -166,52 +179,83 @@ class _ChatScreenState extends State<ChatScreen> {
 class Message extends StatelessWidget {
   final DataSnapshot snapshot;
   final Animation animation;
+
   Message({this.snapshot, this.animation});
 
   @override
   Widget build(BuildContext context) {
     return new SizeTransition(
-      sizeFactor:
-      new CurvedAnimation(parent: animation, curve: Curves.easeOut),
+      sizeFactor: new CurvedAnimation(parent: animation, curve: Curves.easeOut),
       axisAlignment: 0.0,
       child: new Container(
-        margin: new EdgeInsets.only(left: googleSignIn.currentUser.email==snapshot.value['email']?15.0:5.0,right: googleSignIn.currentUser.email==snapshot.value['email']?5.0:15.0,top: 5.0,bottom: 5.0),
+        margin: new EdgeInsets.only(
+            left: googleSignIn.currentUser.email == snapshot.value['email']
+                ? 25.0
+                : 0.0,
+            right: googleSignIn.currentUser.email == snapshot.value['email']
+                ? 0.0
+                : 25.0,
+            top: 5.0,
+            bottom: 5.0),
         padding: const EdgeInsets.all(10.0),
         decoration: new BoxDecoration(
-          color: Theme.of(context).primaryColor,
-          border: new Border(),
-          borderRadius: new BorderRadius.only(
-            bottomLeft: new Radius.circular(15.0),
-            bottomRight: new Radius.circular(15.0),
-            topLeft: googleSignIn.currentUser.email==snapshot.value['email']?new Radius.circular(15.0):Radius.zero,
-            topRight: googleSignIn.currentUser.email==snapshot.value['email']?Radius.zero:new Radius.circular(15.0)
-          )
-        ),
+            color: Theme.of(context).primaryColor,
+            border: new Border(),
+            borderRadius: new BorderRadius.only(
+                bottomLeft: new Radius.circular(15.0),
+                bottomRight: new Radius.circular(15.0),
+                topLeft:
+                    googleSignIn.currentUser.email == snapshot.value['email']
+                        ? new Radius.circular(15.0)
+                        : Radius.zero,
+                topRight:
+                    googleSignIn.currentUser.email == snapshot.value['email']
+                        ? Radius.zero
+                        : new Radius.circular(15.0))),
         child: new Row(
-          mainAxisAlignment: googleSignIn.currentUser.email==snapshot.value['email']?MainAxisAlignment.end:MainAxisAlignment.start,
+          mainAxisAlignment:
+              googleSignIn.currentUser.email == snapshot.value['email']
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             new Container(
               padding: const EdgeInsets.symmetric(horizontal: 6.0),
               child: new CircleAvatar(
-                backgroundImage: new NetworkImage(snapshot.value['senderPhotoUrl']),
+                backgroundImage:
+                    new NetworkImage(snapshot.value['senderPhotoUrl']),
               ),
             ),
-            new Flexible(child: new Column(
-              mainAxisSize: MainAxisSize.max,
+            new Flexible(
+                child: new Column(
+              mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                new Text(snapshot.value['senderName'],
-                    style: Theme.of(context).textTheme.subhead),
+                new Text(snapshot.value['senderName'] + ":",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .subhead
+                        .copyWith(fontWeight: FontWeight.bold)),
                 new Container(
                   margin: const EdgeInsets.only(top: 5.0),
-                  width: MediaQuery.of(context).size.width*0.7,
-                  child: snapshot.value['imageUrl'] != null ?
-                  new Image.network(                                         
-                    snapshot.value['imageUrl'],
-                    width: 250.0,                                           
-                  ) :  new Text(snapshot.value['text'],),
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: snapshot.value['imageUrl'] != null
+                      ? new Stack(
+                          children: <Widget>[
+                            const SizedBox(
+                              child: const LinearProgressIndicator(),
+                              height: 2.0,
+                            ),
+                            new Image(
+                                image: new CachedNetworkImageProvider(
+                                    snapshot.value["imageUrl"]))
+                          ],
+                        )
+                      : new Text(
+                          snapshot.value['text'],
+                        ),
                 ),
               ],
             ))
@@ -221,5 +265,3 @@ class Message extends StatelessWidget {
     );
   }
 }
-
-
